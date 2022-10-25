5f8ba1413854ef4757f2715085f0064dc9c560f0
Kian Behzad <kian.behzad@gmail.com>
2022-10-25 18:26:36 +0330

    [refactor] refactor Game class

--- include/game.h
+++ include/game.h
@@ -14,6 +14,21 @@ public:
     Game(QObject* parent = nullptr);
     ~Game();
 
+    void reset_board();
+    void reset_round();
+    void reset_game();
+
+    bool step();
+
+    static void print_board();
+
+private:
+    // shortcuts to extern variables
+    Board& board;
+    int& cell_num;
+    Tron& blue;
+    Tron& red;
+
 };
 
 #endif // GAME_H
